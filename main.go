1b606af0f3b5b504d10896b6fc304e2af448d854
Moon <wmoonx@outlook.com>
2021-11-23 21:34:57 +0800

    fix

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"GoBIMS/model"
+	"GoBIMS/routes"
+	"GoBIMS/utils"
 )
 
 func main() {
-	model.InitDB()
+	// model.InitDB()
+	router := routes.InitRouter()
+	router.Run(utils.HttpPort)
 }
--- routes/router.go
+++ routes/router.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"GoBIMS/controllers"
 	"GoBIMS/utils"
 
 	"github.com/gin-gonic/gin"
@@ -9,7 +10,12 @@ import (
 func InitRouter() *gin.Engine {
 	gin.SetMode(utils.AppMode)
 	router := gin.Default()
-	// router.POST("/api/v1/book", controllers.GetBookByID)
-	// router.GET("/api/v1/search", controllers.SearchByName)
+	// 用户模块路由接口
+	router.POST("login/", controllers.Login)
+	router.POST("user/add", controllers.AddUser)
+	router.GET("users", controllers.GetUser)
+	router.PUT("user/", controllers.EditUser)
+	router.DELETE("user/:id", controllers.DeleteUser)
+
 	return router
 }
