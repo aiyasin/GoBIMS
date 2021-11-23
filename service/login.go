5b61167957056a6781658fd691c58c0b6631d129
Moon <wmoonx@outlook.com>
2021-11-23 21:33:11 +0800

    login router&link HTML

---
+++ service/login.go
@@ -0,0 +1,10 @@
+package service
+
+// 用户登录
+// func Login(c *gin.Context) {
+// 	types := c.DefaultPostForm("type", "post")
+// 	username := c.PostForm("username")
+// 	password := c.PostForm("password")
+
+// 	c.String(http.StatusOK, fmt.Sprintf("username:%s , password:%s , types:%s", username, password, types))
+// }
