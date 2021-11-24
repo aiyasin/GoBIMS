92b4d008b7749f368f1f7899493837751a1cea63
Moon <wmoonx@outlook.com>
2021-11-24 23:30:38 +0800

    add crypt password

--- model/User.go
+++ model/User.go
@@ -2,7 +2,10 @@ package model
 
 import (
 	"GoBIMS/utils/errmsg"
+	"log"
+	"math"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -13,28 +16,28 @@ type User struct {
 	Role     int    `gorm:"type:int;default:2;columns:role" json:"role" validate:"required" label:"角色码"`
 }
 
-// 查询用户是否存在
+// CheckUser 查询用户是否存在
 func CheckUser(user User) (code int) {
 	db.Select("id").Where("user_name = ?", user.UserName).First(&user)
 	if len(user.PassWord) < 6 {
-		return errmsg.ERROR_PASSWORD_LESS_THAN6
+		return errmsg.ErrorPasswordLessThan6
 	}
 	if user.ID > 0 {
-		return errmsg.ERROR_USERNAME_USED //1001
+		return errmsg.ErrorUsernameUsed //1001
 	}
-	return errmsg.SUCCSE //200
+	return errmsg.SUCCESS //200
 }
 
-// 新增用户
+// CreatUser 新增用户
 func CreatUser(user *User) (code int) {
 	err := db.Create(&user).Error
 	if err != nil {
 		return errmsg.ERROR //500
 	}
-	return errmsg.SUCCSE //200
+	return errmsg.SUCCESS //200
 }
 
-// 查询用户列表
+// CheckUserPage 查询用户列表
 func CheckUserPage(username string, pageSize int, pageNum int) ([]User, int64) {
 	var user []User
 	var total int64
@@ -45,7 +48,8 @@ func CheckUserPage(username string, pageSize int, pageNum int) ([]User, int64) {
 		db.Model(&user).Where(
 			"user_name LIKE ?", username+"%",
 		).Count(&total)
-		return user, total
+		totalnum := int64(math.Ceil(float64(total) / float64(pageSize)))
+		return user, totalnum
 	}
 	db.Select("id,user_name,role,created_at").Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&user)
 	db.Model(&user).Count(&total)
@@ -53,5 +57,28 @@ func CheckUserPage(username string, pageSize int, pageNum int) ([]User, int64) {
 	if err != nil {
 		return user, 0
 	}
-	return user, total
+	totalnum := int64(math.Ceil(float64(total) / float64(pageSize)))
+	return user, totalnum
+}
+
+// BeforeCreate 密码加密&权限控制
+func (u *User) BeforeCreate(_ *gorm.DB) (err error) {
+	u.PassWord = ScryptPassWord(u.PassWord)
+	u.Role = 2
+	return nil
+}
+
+func (u *User) BeforeUpdate(_ *gorm.DB) (err error) {
+	u.PassWord = ScryptPassWord(u.PassWord)
+	return nil
+}
+
+// ScryptPassWord 密码加密
+func ScryptPassWord(password string) string {
+	const cost = 10
+	HashPassWord, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(HashPassWord)
 }
